b280fdd62caf81348522a040e92cc9c55d033b09
willaanov <willanovansatritamma@gmail.com>
2026-08-11 09:46:44 +0700

    menambahkan app get pada server

--- glowlist-backend/server.js
+++ glowlist-backend/server.js
@@ -11,3 +11,36 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server GlowList jalan di http://localhost:${PORT}`)
 });
+
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost' ,
+    user: 'root',
+    password: '',
+    database: 'glowlist_db'
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Gagal konek ke database:', err)
+    } else {
+        console.log('Berhasil konek ke database GlowList');
+    }
+})
+
+app.get('/produk', (req, res) => {
+    const sql = 'SELECT * FROM produk';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error:err });
+        res.json(results);
+    });
+});
+
+app.get('/kategori', (req, res) => {
+    const sql = 'SELECT * FROM kategori';
+    db.query(sql, (err, results) => {
+        if(err) return res.status(500).json({ error:err })
+            res.json(results);
+    });
+});
